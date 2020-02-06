07bf58442e8ef578c133f2b8a5a9fa5f281c46fe
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-05 23:13:09 -0600

    Update degrees celsius.py

--- degrees celsius.py
+++ degrees celsius.py
@@ -1,5 +1,13 @@
 #This program was written by Jordan on 2/2/20
 # This program converts Farenheit to Celsius 
+#You must have noticed the program doesn't run.
+#Here is one way of coding it -- try this:
+#degreesF = int(input("What is the temperature in Fahrenheit? "))
+
+#degreesC = (degreesF - 32) * 5 / 9
+
+#print("The temperature in Celsius is ", degreesC)
+
 
 far = "degrees fahrenheit"
 cel = "degrees celsius"
