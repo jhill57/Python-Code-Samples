0d754419f618dc02a4d67cb345c7c4245aece621
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-05 23:06:03 -0600

    Update mpg .py

--- mpg .py
+++ mpg .py
@@ -1,6 +1,6 @@
 #This program was written by Jordan on 2/2/20
 #This program computes MPG for a car
-
+#Consider using float data type instead of integer. This way your program can handle fraction of a mile/gallor as well.
 miles = int(input("How many miles?"))
 gallons = int(input("How many gallons used?"))
 mpg = miles/ gallons
